a444164ede6cd615c87da3f6624fd40c144fc17f
UTandor <demonsouls34865@gmail.com>
2023-06-05 20:55:16 +0500

    There is a UI placeholder. The place holder can place new objects, the new objects have same properties as the old square. Snapping to grid works correctly

--- grid.py
+++ grid.py
@@ -4,15 +4,17 @@ pygame.init()
 
 window_size = (800, 600)
 screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption("Testing..........")
+pygame.display.set_caption("Grid Game")
 
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
-HOVER_COLOR = (0, 255, 0)  # Example color for hover
-CLICK_COLOR = (0, 0, 255)  # Blue color for clicked state
+HOVER_COLOR = (0, 255, 0)
+CLICK_COLOR = (0, 0, 255)
+UI_BACKGROUND_COLOR = (200, 200, 200)
+UI_OUTLINE_COLOR = BLACK
 
-class Rectangle():
+class Rectangle:
     def __init__(self, scale, x_position, y_position, rect_color):
         self.scale = scale
         self.x_position = x_position
@@ -41,54 +43,99 @@ class Rectangle():
             mouse_pos = pygame.mouse.get_pos()
             rectangle_rect = pygame.Rect(self.x_position, self.y_position, self.scale, self.scale)
             if rectangle_rect.collidepoint(mouse_pos):
+                if not self.is_dragging:  
+                    self.x_position = mouse_pos[0] - self.scale // 2
+                    self.y_position = mouse_pos[1] - self.scale // 2
                 self.is_dragging = True
                 self.offset_x = self.x_position - mouse_pos[0]
                 self.offset_y = self.y_position - mouse_pos[1]
                 self.rect_color = CLICK_COLOR
         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            self.is_dragging = False
+            if self.is_dragging:
+                self.is_dragging = False
+                if self.rect_color == CLICK_COLOR:  
+                    k = 50
+                    self.x_position = round(self.x_position / k) * k
+                    self.y_position = round(self.y_position / k) * k
         elif event.type == pygame.MOUSEMOTION:
             if self.is_dragging:
                 mouse_pos = pygame.mouse.get_pos()
                 self.x_position = mouse_pos[0] + self.offset_x
                 self.y_position = mouse_pos[1] + self.offset_y
 
-                # Snap the rectangle to the grid
-                k = 50  # Size of each grid square
-                self.x_position = round(self.x_position / k) * k
-                self.y_position = round(self.y_position / k) * k
+class UIElement:
+    def __init__(self, x_position, y_position, width, height, background_color, outline_color):
+        self.x_position = x_position
+        self.y_position = y_position
+        self.width = width
+        self.height = height
+        self.background_color = background_color
+        self.outline_color = outline_color
 
-# Create an instance of the Rectangle class
-k = 50  # Size of each grid square
-x_position = ((window_size[0] // k) // 2) * k  # Calculate x-coordinate for middle tile
-y_position = ((window_size[1] // k) // 2) * k  # Calculate y-coordinate for middle tile
+    def render(self):
+        pygame.draw.rect(screen, self.outline_color, (self.x_position, self.y_position, self.width, self.height), 3)
+        pygame.draw.rect(screen, self.background_color, (self.x_position + 3, self.y_position + 3, self.width - 6, self.height - 6))
+
+    def check_hover(self):
+        mouse_pos = pygame.mouse.get_pos()
+        element_rect = pygame.Rect(self.x_position, self.y_position, self.width, self.height)
+        if element_rect.collidepoint(mouse_pos):
+            self.outline_color = HOVER_COLOR
+        else:
+            self.outline_color = BLACK
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = pygame.mouse.get_pos()
+            element_rect = pygame.Rect(self.x_position, self.y_position, self.width, self.height)
+            if element_rect.collidepoint(mouse_pos):
+                rectangle = Rectangle(50, mouse_pos[0], mouse_pos[1], RED)
+                rectangles.append(rectangle)
+
+k = 50
+x_position = ((window_size[0] // k) // 2) * k
+y_position = ((window_size[1] // k) // 2) * k
 rectangle = Rectangle(k, x_position, y_position, BLACK)
 
+ui_element = UIElement(window_size[0] - 120, window_size[1] - 120, 100, 100, UI_BACKGROUND_COLOR, UI_OUTLINE_COLOR)
+
+rectangles = []
+
 running = True
+clock = pygame.time.Clock()
+
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        rectangle.handle_event(event)
+        ui_element.handle_event(event)
+
     screen.fill(WHITE)
 
-    # Draw the grid
     for x in range(0, window_size[0], k):
         pygame.draw.line(screen, (0, 0, 0, 100), (x, 0), (x, window_size[1]), 1)
     for y in range(0, window_size[1], k):
         pygame.draw.line(screen, (0, 0, 0, 100), (0, y), (window_size[0], y), 1)
 
+    for rect in rectangles:
+        rect.render()
+        rect.check_hover()
+        rect.handle_event(event)
+
     rectangle.render()
     rectangle.check_hover()
 
+    ui_element.render()
+    ui_element.check_hover()
+
     font = pygame.font.Font(None, 36)
-    text = font.render("You should be able to drag", True, BLACK)
-    text_x = 10  # X-coordinate for top-left corner
-    y_position = 10  # Y-coordinate for top-left corner
-
-    screen.blit(text, (text_x, y_position))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        rectangle.handle_event(event)
+    text = font.render("You can drag rectangles", True, BLACK)
+    text_x = 10
+    text_y = 10
+    screen.blit(text, (text_x, text_y))
 
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
