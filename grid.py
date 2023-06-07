f6b35fcb552dc0ce03ff8772f6b0e4cd1099acc3
Utnator <118050517+UTandor@users.noreply.github.com>
2023-06-07 18:18:46 +0500

    Update grid.py
    In the last commit, I told Chat GPT to write some comments. Removed 'em as they make my work look AI generated. That's all!

--- grid.py
+++ grid.py
@@ -1,10 +1,8 @@
 import pygame
 import math
 
-# Initialize Pygame
 pygame.init()
 
-# Define constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 GRID_SIZE = 50
@@ -19,15 +17,12 @@ UI_WIDTH = 200
 UI_HEIGHT = 50
 UI_TOGGLE_TEXT = "Toggle UI (T)"
 
-# Create the Pygame window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Road Simulation")
 
-# Load road image and scale it down
 road_image = pygame.image.load(ROAD_IMAGE_PATH)
 road_image = pygame.transform.scale(road_image, (ROAD_SIZE, ROAD_SIZE))
 
-# Define road object class
 class RoadObject:
     def __init__(self, x, y):
         self.x = x
@@ -45,72 +40,63 @@ class RoadObject:
     def rotate(self):
         self.rotation = (self.rotation + 90) % 360
 
-# Create road objects list
 road_objects = []
 
-# Initialize variables
 ui_visible = True
 creating_road = False
 
-# Game loop
 running = True
 while running:
-    # Handle events
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # Left mouse button
+            if event.button == 1:  
                 if ui_visible:
                     if not creating_road:
-                        # Start creating road object
+
                         creating_road = True
                         road_preview = RoadObject(*event.pos)
                     else:
-                        # Place road object on the grid
+
                         road_preview.snap_to_grid()
                         road_objects.append(road_preview)
                         creating_road = False
                 else:
-                    # Place road object on the grid directly
+
                     road_objects.append(RoadObject(*event.pos))
-            elif event.button == 3:  # Right mouse button
+            elif event.button == 3:  
                 if not creating_road and not ui_visible:
-                    # Delete road object
+
                     for obj in road_objects:
                         if obj.x == event.pos[0] and obj.y == event.pos[1]:
                             road_objects.remove(obj)
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_t:
-                # Toggle UI visibility
+
                 ui_visible = not ui_visible
             elif event.key == pygame.K_r:
-                # Rotate road preview
+
                 if creating_road and ui_visible:
                     road_preview.rotate()
 
-    # Update road preview position
     if creating_road:
         road_preview.x, road_preview.y = pygame.mouse.get_pos()
 
-    # Draw the background
     screen.fill(BACKGROUND_COLOR)
 
-    # Draw the grid
     for x in range(0, SCREEN_WIDTH, GRID_SIZE):
         pygame.draw.line(screen, GRID_COLOR, (x, 0), (x, SCREEN_HEIGHT))
     for y in range(0, SCREEN_HEIGHT, GRID_SIZE):
         pygame.draw.line(screen, GRID_COLOR, (0, y), (SCREEN_WIDTH, y))
 
-    # Draw the road objects
     for obj in road_objects:
         obj.draw()
 
-    # Draw the road preview
     if creating_road:
         road_preview.draw()
 
-    # Draw the UI element
     if ui_visible:
         pygame.draw.rect(screen, UI_COLOR, (0, 0, UI_WIDTH, UI_HEIGHT))
         pygame.draw.rect(screen, UI_BORDER_COLOR, (0, 0, UI_WIDTH, UI_HEIGHT), 2)
@@ -118,8 +104,6 @@ while running:
         text = font.render(UI_TOGGLE_TEXT, True, (0, 0, 0))
         screen.blit(text, (10, 10))
 
-    # Update the display
     pygame.display.flip()
 
-# Quit the simulation
 pygame.quit()
